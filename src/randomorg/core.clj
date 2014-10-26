73b28de5fc810c2589ecbe50fbcf68ed172f7382
mishadoff <mishadoff@gmail.com>
2014-10-26 23:24:23 +0200

    Generate Strings

--- src/randomorg/core.clj
+++ src/randomorg/core.clj
@@ -1,7 +1,7 @@
 (ns randomorg.core
   (:require [clojure.data.json :as json]
             [clj-http.client :as http]
-            [randomorg.validator :as vv]))
+            [randomorg.validator :as v]))
 
 (def API_ENDPOINT "https://api.random.org/json-rpc/1/invoke")
 
@@ -67,45 +67,45 @@
    base - base for numbers, default is 10
 "
   [& {:keys [n min max replacement base]
-      :as request-data}]
-  (let [request-data (merge {:replacement true :base 10} request-data)]
-    (validate request-data
-              :n vv/n-validator
-              :min vv/range-1e9-validator
-              :max vv/range-1e9-validator
-              :replacement vv/boolean
-              :base vv/base-validator)
-
-    (request-processor "generateIntegers"
-                       (select-keys request-data [:n :min :max :replacement :base]))))
+      :as raw-request-data}]
+  (let [request-data (-> (merge {:replacement true :base 10} raw-request-data)
+                         (select-keys [:n :min :max :replacement :base]))]
+    (v/validate request-data
+                :n v/n-validator
+                :min v/range-1e9-validator
+                :max v/range-1e9-validator
+                :replacement v/boolean
+                :base v/base-validator)
+    
+    (request-processor "generateIntegers" request-data)))
 
 
 (defn generate-decimal-fractions
   "Generates decimal fractions from a uniform distribution across the [0, 1] interval
 
    Required Parameters:
-   n - number of integers, [1, 1e4]
+   n - number of decimals, [1, 1e4]
    decimalPlaces - number of decimal places, [1, 20]
 
    Optional Parameters:
    replacement - true may return duplicates, false return all unique numbers, default is true
 "
   [& {:keys [n decimalPlaces replacement]
-      :as request-data}]
-  (let [request-data (merge {:replacement true} request-data)]
-    (validate request-data
-              :n vv/n-validator
-              :decimalPlaces vv/decimal-range-validator
-              :replacement vv/boolean)
+      :as raw-request-data}]
+  (let [request-data (-> (merge {:replacement true} raw-request-data)
+                         (select-keys [:n :decimalPlaces :replacement]))]
+    (v/validate request-data
+                :n v/n-validator
+                :decimalPlaces v/decimal-range-validator
+                :replacement v/boolean)
     
-    (request-processor "generateDecimalFractions"
-                       (select-keys request-data [:n :decimalPlaces :replacement]))))
+    (request-processor "generateDecimalFractions" request-data)))
 
 (defn generate-gaussians
   "Generates decimal fractions from a normal distribution
 
    Required Parameters:
-   n - number of integers, [1, 1e4]
+   n - number of decimals, [1, 1e4]
    mean - mean of distribution, [-1e6, 1e6]
    standardDeviation - standard deviation, [-1e6, 1e6]
    significantDigits - significant digits, [2, 20]
@@ -114,12 +114,35 @@
    None
 "
   [& {:keys [n mean standardDeviation significantDigits]
-      :as request-data}]
-  (validate request-data
-            :n vv/n-validator
-            :mean vv/range-1e6-validator
-            :standardDeviation vv/range-1e6-validator
-            :significantDigits vv/significant-digits-validator)
-  
-  (request-processor "generateGaussians"
-                     (select-keys request-data [:n :mean :standardDeviation :significantDigits])))
+      :as raw-request-data}]
+  (let [request-data (select-keys raw-request-data [:n :mean :standardDeviation :significantDigits])]
+    (v/validate request-data
+              :n v/n-validator
+              :mean v/range-1e6-validator
+              :standardDeviation v/range-1e6-validator
+              :significantDigits v/significant-digits-validator)
+    
+    (request-processor "generateGaussians" request-data)))
+
+(defn generate-strings
+  "Generates random strings from specified characters
+
+   Required Parameters:
+   n - number of strings, [1, 1e4]
+   length - the length of each string, [1, 20]
+   characters - string that contains set of characters, [1, 80]
+
+   Optional Parameters:
+   replacement - true may return duplicates, false return all unique strings, default is true
+"
+  [& {:keys [n length characters replacement]
+      :as raw-request-data}]
+  (let [request-data (-> (merge {:replacement true} raw-request-data)
+                         (select-keys [:n :length :characters :replacement]))]
+    (v/validate request-data
+              :n v/n-validator
+              :length v/string-length-validator
+              :characters v/characters-validator
+              :replacement v/boolean)
+    
+    (request-processor "generateStrings" request-data)))
--- src/randomorg/validator.clj
+++ src/randomorg/validator.clj
@@ -22,14 +22,26 @@
   (or (= false value)
       (= true value)))
 
+(v/defvalidator string
+  {:default-message-format "%s must be a string"}
+  [value]
+  (string? value))
+
 (v/defvalidator ranged
   {:default-message-format "%s must be in specified range"}
   [value [from to]]
   (<= from value to))
 
+(v/defvalidator string-ranged
+  {:default-message-format "%s must be in specified range"}
+  [value [from to]]
+  (<= from (count value) to))
+
 (def n-validator [v/required integer [ranged [1 1e4]]])
 (def range-1e9-validator [v/required integer [ranged [-1e9 1e9]]])
 (def range-1e6-validator [v/required integer [ranged [-1e6 1e6]]])
 (def decimal-range-validator [v/required integer [ranged [1 20]]])
+(def string-length-validator [v/required integer [ranged [1 20]]])
 (def significant-digits-validator [v/required integer [ranged [2 20]]])
 (def base-validator [[v/member #{2 8 10 16} :message "base must be 2, 8, 10 or 16"]])
+(def characters-validator [v/required string [string-ranged [1 20]]])
